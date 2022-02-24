3b1bc9a470e735c06a3bb10cfc4c03a954991e65
jayebrady <Jaye.Brady@mdtplt1109.lan>
2022-02-24 16:34:18 +0000

    added styling to custom buttons and added signup link

--- Screens/SignInScreen/userLogin.js
+++ Screens/SignInScreen/userLogin.js
@@ -1,33 +1,64 @@
-import { Button, ImageBackground, Text, TouchableOpacity, View, StyleSheet, Image} from "react-native";
+import {
+  Button,
+  ImageBackground,
+  Text,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+  Image,
+} from "react-native";
 import { useRef, useState, useEffect } from "react";
 import CustomInput from "../../Components/CustomInput/CustomInput";
 import CustomButton from "../../Components/customButton/customButton";
 
 const UserLogin = () => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const onSignIn = () => {
-        console.warn('it worked')
-    }
+  const onSignIn = () => {
+    console.warn("Sign in.");
+  };
+  const onForgotPassword = () => {
+    console.warn("Forgot Password.");
+  };
+  const onSignUp = () => {
+    console.warn("Sign up.");
+  };
+  return (
+    <View style={styles.root}>
+      <Text>User Login Page</Text>
+      <CustomInput
+        placeholder="Username"
+        value={username}
+        setValue={setUsername}
+      />
+      <CustomInput
+        placeholder="Password"
+        value={password}
+        setValue={setPassword}
+        secureTextEntry={true}
+      />
 
-    return (
-        <View style={styles.root}>
-            <Text>User Login Page</Text>
-            <CustomInput placeholder='Username' value={username} setValue={setUsername} />
-            <CustomInput placeholder='Password' value={password} setValue={setPassword} secureTextEntry={true} />
-
-            <CustomButton text={'Sign In'} onPress={onSignIn}/>
-        </View>
-    )
-}
+      <CustomButton text={"Sign In"} onPress={onSignIn} />
+      <CustomButton
+        text={"Forgot Password?"}
+        onPress={onForgotPassword}
+        type="TERTIARY"
+      />
+      <CustomButton
+        text="Don't have an account? Create one"
+        onPress={onSignUp}
+        type="TERTIARY"
+      ></CustomButton>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    root: {
-        alignItems: 'center',
-        padding: 20, 
-        
-    }
-})
+  root: {
+    alignItems: "center",
+    padding: 20,
+  },
+});
 
 export default UserLogin;
