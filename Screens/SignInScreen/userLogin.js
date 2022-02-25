51e7e974a1c43db489919287c4eab0a942cc5e25
jayebrady <Jaye.Brady@mdtplt1109.lan>
2022-02-25 16:37:46 +0000

    add navigation to signup page

--- Screens/SignInScreen/userLogin.js
+++ Screens/SignInScreen/userLogin.js
@@ -10,11 +10,8 @@ import {
 import { useRef, useState, useEffect } from "react";
 import CustomInput from "../../Components/CustomInput/CustomInput";
 import CustomButton from "../../Components/customButton/customButton";
-
 import { useNavigation, userNavigation } from "@react-navigation/native";
-
 import { login } from "../../api";
-
 
 const UserLogin = () => {
   const [email, setEmail] = useState("");
@@ -23,40 +20,28 @@ const UserLogin = () => {
   const navigation = useNavigation();
 
   const onSignIn = () => {
-
     console.warn("Sign in.");
-    // validate user
-    navigation.navigate("HomeScreen"); // Home Page Placeholder
+    navigation.navigate("HomeScreen");
 
     login().then((res) => {
       // alert(res)
       console.warn(res);
-    })
-    // console.warn("Sign in.");
+    });
+  };
 
-  };
-  const onForgotPassword = () => {
-    console.warn("Forgot Password.");
-    navigation.navigate("ForgotPassword"); // Add Forgot Password screen to navigation/index.js
-  };
   const onSignUp = () => {
     console.warn("Sign up.");
-    navigation.navigate("SignUp"); // Add Forgot Password screen to navigation/index.js
+    navigation.navigate("SignUp");
   };
   /*
-  if user_id !== Num {
-      
+  if user_id !== Num {   
   }
   */
 
   return (
     <View style={styles.root}>
       <Text>User Login Page</Text>
-      <CustomInput
-        placeholder="Username"
-        value={email}
-        setValue={setEmail}
-      />
+      <CustomInput placeholder="Username" value={email} setValue={setEmail} />
       <CustomInput
         placeholder="Password"
         value={password}
@@ -65,11 +50,6 @@ const UserLogin = () => {
       />
 
       <CustomButton text={"Sign In"} onPress={onSignIn} />
-      <CustomButton
-        text={"Forgot Password?"}
-        onPress={onForgotPassword}
-        type="TERTIARY"
-      />
       <CustomButton
         text="Don't have an account? Create one"
         onPress={onSignUp}
