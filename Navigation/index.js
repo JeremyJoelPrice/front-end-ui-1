c2962a43a89420eed135e432f5292787c0f81194
jayebrady <Jaye.Brady@mdtplt1109.lan>
2022-02-28 16:52:26 +0000

    add fact card

--- Navigation/index.js
+++ Navigation/index.js
@@ -6,6 +6,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import UserLogin from "../Screens/SignInScreen/userLogin";
 import SignUp from "../Screens/SignUpScreen/signUp";
 import HomeScreen from "../Screens/HomeScreen/HomeScreen";
+import FactCard from "../Screens/FactCardScreen/factCard";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,9 +14,10 @@ const Navigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="UserLogin" component={UserLogin} />
+        <Stack.Screen name="FactCard" component={FactCard} />
+        {/* <Stack.Screen name="UserLogin" component={UserLogin} />
         <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
