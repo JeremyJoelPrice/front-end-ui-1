3a7173db5b4c13517535be58d44bd282dc7274be
Jeremy Price <jeremy@jeremys-imac.home>
2022-02-28 18:11:51 +0000

    save a file I forgot to save before the previous commit

--- Components/Styled Components/index.js
+++ Components/Styled Components/index.js
@@ -2,5 +2,5 @@ export * from "./containers";
 export * from "./text";
 export * from "./navbar";
 export * from "./buttons";
-export * from "./OneBird";
+export * from "./oneBird";
 export * from "./images";
