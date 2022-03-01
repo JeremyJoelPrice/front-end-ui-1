217d5926a60dd755f94a9a234a86fa143a60eca1
jayebrady <Jaye.Brady@mdtplt1109.lan>
2022-03-01 12:37:47 +0000

    add fact card functionality

--- Screens/FactCardScreen/factCard.js
+++ Screens/FactCardScreen/factCard.js
@@ -1,59 +1,28 @@
-import { Text, View, StyleSheet, Image } from "react-native";
+import { useContext, useEffect, useState } from "react";
+import {
+  HeaderPanel,
+  HeaderText,
+  LargeImage,
+  Text,
+} from "../../Components/Styled Components";
+import { getFactCard, getFactPhoto } from "../../api";
 
 const FactCard = () => {
+  const [cardFact, setCardFact] = useState([]);
+
+  useEffect(() => {
+    getCardFact(userId).then((card) => {
+      setCardFact(card);
+    });
+  }, []);
+
   return (
-    <View style={styles.root}>
-      <Text style={styles.title}>Fact Card</Text>
-      <View style={styles.container}>
-        <Image
-          source={require("./7af1437141037ffc39b8571dd273b2c8.jpeg")}
-          style={styles.image}
-        />
-        <Text style={styles.text}>
-          Eiusmod enim pariatur labore reprehenderit in aliquip. Eu amet tempor
-          nostrud id excepteur qui veniam consectetur. Elit quis eiusmod aute
-          deserunt cupidatat fugiat. Exercitation dolore magna laborum enim
-          consectetur anim nisi ipsum nisi. Qui eiusmod mollit laborum
-          incididunt sit laboris laborum sit. Dolore anim elit cupidatat
-          reprehenderit laborum in. Veniam consequat minim excepteur anim id
-          incididunt ut.
-        </Text>
-      </View>
-    </View>
+    <HeaderPanel>
+      <HeaderText>{factCard.bird_name}</HeaderText>
+      <LargeImage source={getFactPhoto} />
+      <Text>{factCard.fact}</Text>
+    </HeaderPanel>
   );
 };
-
-const styles = StyleSheet.create({
-  root: {
-    alignItems: "center",
-    padding: 20,
-  },
-  title: {
-    fontSize: 30,
-    fontWeight: "bold",
-    margin: 10,
-    color: "black",
-  },
-  container: {
-    alignItems: "center",
-    height: 500,
-    width: 400,
-    borderWidth: 5,
-    borderRadius: 20,
-    padding: 20,
-    backgroundColor: "grey",
-  },
-  image: {
-    width: 250,
-    height: 200,
-    borderWidth: 3,
-    borderRadius: 5,
-  },
-  text: {
-    padding: 20,
-    marginTop: 20,
-    backgroundColor: "white",
-  },
-});
 
 export default FactCard;
--- api.js
+++ api.js
@@ -1,21 +1,32 @@
-
 const axios = require("axios").create({
-	baseURL: "https://bird-brain-nc-project.herokuapp.com"
-	// baseURL: "http://localhost:8080"
+  baseURL: "https://bird-brain-nc-project.herokuapp.com",
+  // baseURL: "http://localhost:8080"
 });
 
 export const login = (email, password) => {
-	return axios.get("/login", {
-		params: {
-			email,
-			password
-		}
-	});
+  return axios.get("/login", {
+    params: {
+      email,
+      password,
+    },
+  });
 };
 
 export const signup = (email, password) => {
-	return axios.post("/login", {
-		email,
-		password
-	});
+  return axios.post("/login", {
+    email,
+    password,
+  });
+};
+
+export const getFactCard = (user_id) => {
+  return axios.get(`/users/${user_id}/fact_cards/`).then((res) => {
+    return res.data.cards;
+  });
+};
+
+export const getFactPhoto = (bird_name, image_num) => {
+  return axios.get(`/photo/${bird_name}/${image_num}`).then((res) => {
+    return res.data;
+  });
 };
