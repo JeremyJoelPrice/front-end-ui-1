3b895604a9fbca65febbc0f3143f9a82fef02e83
Jeremy Price <jeremy@jeremys-imac.home>
2022-03-02 13:12:40 +0000

    restore working version

--- Screens/AviaryScreen/Aviary.js
+++ Screens/AviaryScreen/Aviary.js
@@ -1,12 +1,12 @@
 import { useContext, useEffect, useState } from "react";
 import { v4 as uuid } from "uuid";
 import {
-  AppContainer,
-  HeaderPanel,
-  HeaderText,
-  MainContent,
-  StyledBirdCardList,
-  View,
+	AppContainer,
+	HeaderPanel,
+	HeaderText,
+	MainContent,
+	StyledBirdCardList,
+	View
 } from "../../Components/Styled Components";
 import BirdCard from "./BirdCard";
 import FactCardsContext from "../../FactCardsContext";
@@ -16,29 +16,34 @@ import { extractBirdCards } from "../../utils";
 import { Navbar } from "../../Components/Navbar/Navbar";
 
 const Aviary = ({ navigation }) => {
-  const { factCards, setFactCards } = useContext(FactCardsContext);
-  const { userId } = useContext(UserContext);
-  const [birdCards, setBirdCards] = useState([]);
+	const { factCards, setFactCards } = useContext(FactCardsContext);
+	const { currentUser } = useContext(UserContext);
+	const [birdCards, setBirdCards] = useState([]);
 
-  useEffect(() => {
-    getFactCards(userId).then((cards) => {
-      setFactCards(cards);
-      setBirdCards(extractBirdCards(cards));
-    });
-  }, []);
+	useEffect(() => {
+		getFactCards(currentUser.userId).then((cards) => {
+			setFactCards(cards);
+			setBirdCards(extractBirdCards(cards));
+		});
+	}, []);
 
-  return (
-    <>
-      <View>
-        <HeaderText>User's Aviary</HeaderText>
-      </View>
-      <View>
-        {birdCards.map((card) => {
-          return <BirdCard key={uuid()} navigation={navigation} card={card} />;
-        })}
-      </View>
-    </>
-  );
+	return (
+		<AppContainer>
+			<HeaderPanel>
+				<HeaderText>{currentUser.username}'s Aviary</HeaderText>
+			</HeaderPanel>
+			<MainContent>
+				<StyledBirdCardList>
+					{birdCards.map((card) => {
+						return (
+							<BirdCard key={uuid()} navigation={navigation} card={card} />
+						);
+					})}
+				</StyledBirdCardList>
+			</MainContent>
+			<Navbar navigation={navigation} />
+		</AppContainer>
+	);
 };
 
 export default Aviary;
