aaa4d802e91824656b72940496a0ab555eaafc02
Jeremy Price <jeremy@jeremys-imac.home>
2022-03-03 18:33:14 +0000

    Create loading screen

--- Screens/PhotoUploadScreen/PhotoUpload.js
+++ Screens/PhotoUploadScreen/PhotoUpload.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Navbar } from "../../Components/Navbar/Navbar";
 import {
 	AppContainer,
@@ -6,34 +7,60 @@ import {
 	FormContainer,
 	HeaderPanel,
 	HeaderText,
+	LargeImage,
 	MainContent,
 	Text,
+	ThumbnailImage,
 	UserPhoto,
 	VerticalBuffer
 } from "../../Components/Styled Components";
 
 const PhotoUpload = ({ navigation }) => {
+	const [loading, setLoading] = useState(false);
+
+	function onPress() {
+		setLoading((loading) => !loading);
+	}
+
 	return (
 		<AppContainer>
-			<HeaderPanel>
-				<HeaderText>Photo Upload</HeaderText>
-			</HeaderPanel>
-			<MainContent>
-				<VerticalBuffer />
-				<FlexRow>
-					<UserPhoto source="https://tasteofenglishtea.files.wordpress.com/2012/10/octpp1-cardinal.jpg" />
-				</FlexRow>
-				<FormContainer>
-					<FlexRow>
-						<FormButton>
-							<Text>Submit</Text>
-						</FormButton>
-						<FormButton>
-							<Text>Retake</Text>
-						</FormButton>
-					</FlexRow>
-				</FormContainer>
-			</MainContent>
+			{loading ? (
+				<>
+					<HeaderPanel>
+						<HeaderText>Photo Submitted</HeaderText>
+					</HeaderPanel>
+					<MainContent>
+						<VerticalBuffer />
+						<Text>Fetching results...</Text>
+						<VerticalBuffer />
+						<FlexRow>
+							<ThumbnailImage source={require("../../loading-wheel.gif")} />
+						</FlexRow>
+					</MainContent>
+				</>
+			) : (
+				<>
+					<HeaderPanel>
+						<HeaderText>Photo Upload</HeaderText>
+					</HeaderPanel>
+					<MainContent>
+						<VerticalBuffer />
+						<FlexRow>
+							<UserPhoto source="https://tasteofenglishtea.files.wordpress.com/2012/10/octpp1-cardinal.jpg" />
+						</FlexRow>
+						<FormContainer>
+							<FlexRow>
+								<FormButton onPress={onPress}>
+									<Text>Submit</Text>
+								</FormButton>
+								<FormButton>
+									<Text>Retake</Text>
+								</FormButton>
+							</FlexRow>
+						</FormContainer>
+					</MainContent>
+				</>
+			)}
 			<Navbar navigation={navigation} />
 		</AppContainer>
 	);
